34a1774315fcd586780a19e34dedecfc7733dbbd
Marek Sivak <mmareq@gmail.com>
2019-12-19 12:18:13 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Marek Sivák</title>
+    <meta name="description" content="Marek Sivák is a Product Designer based in Hong Kong.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="theme-color" content="#ffffff">
+	<meta property="og:image" content="img/ms-cover.png" />
+	<meta property="og:title" content="Marek Sivák" />
+    <meta property="og:description" content="Marek Sivák is a Product Designer based in Hong Kong" />
+    <meta property="og:author" content="Marek Sivák is a Product Designer based in Hong Kong." />
+	
+
+
+      <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW" />
+     
+
+    <link href="img/ms-icon.jpg" sizes="192x192" rel="icon" />
+    <link rel="apple-touch-icon" href="img/ms-icon.png">
+ 
+    <link rel="stylesheet" href="css/mareksiv.css" type="text/css" />
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" />
+
+</head>
+<body>
+    <div class="content">
+        <h1>Marek Sivák</h1>
+        <h3>Product Designer based in Hong&nbsp;Kong currently enhancing customer experience at <a target=_blank href="https://lalamove.com">Lalamove</a>.</h3>
+        
+        <div class="list-group">
+            <h5>Active in projects</h5>
+            <ul>
+                <li><a target=_blank href="https://foundlo.st">Foundlost</a></li>
+                <li><a target=_blank href="https://dribbble.com/shots/8553886-Closer-111">Closer</a></li>
+                <li><a target=_blank href="https://mareksivak.github.com/knizky">Knižky</a></li>
+                <li><a target=_blank href="https://levicekedysiadnes.sk">Levice: Kedysi a dnes</a></li>
+            </ul>
+        </div>
+
+        <div class="list-group">
+            <h5>Places to find me</h5>
+            <ul>
+                <li><a target=_blank href="https://instagram.com/mareksiv">Instagram</a></li>
+                <li><a target=_blank href="https://twitter.com/leto">Twitter</a></li>
+                <li><a target=_blank href="https://dribbble.com/leto">Dribbble</a></li>
+                <li><a href="mailto: xsivak@gmail.com">Email</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154783588-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154783588-1');
+</script> 
+    
+</body>
+</html>
